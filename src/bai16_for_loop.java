2c837264b8dc9c9264b15cb62a0155b010f1063a
Ami Pham <amipham@gmail.com>
2026-09-21 14:28:35 +0700

    feat: bai16 - Vòng lặp for

---
+++ src/bai16_for_loop.java
@@ -0,0 +1,8 @@
+public class bai16_for_loop {
+    public static void main(String[] args) {
+        //for ([khoi tao; [dieu kien]; [buoc lap])
+        for(int i = 12; i > 0; i-=2){
+            System.out.println("i = "+ i);
+        }
+    }
+}
