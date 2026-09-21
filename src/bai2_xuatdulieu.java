d730fb49849e5008728a59d2e617323e08962c98
Ami Pham <amipham@gmail.com>
2026-09-21 14:27:52 +0700

    feat: bai2 - Xuất dữ liệu

---
+++ src/bai2_xuatdulieu.java
@@ -0,0 +1,7 @@
+public class bai2_xuatdulieu {
+    public static void main(String[] args) {
+        System.out.println("\"dong 1\"");
+        System.out.println("\ndong 2");
+        System.out.println("\tdong 3");
+    }
+}
