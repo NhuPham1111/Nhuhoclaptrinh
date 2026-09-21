70dc92dadbd5dc3968a9f1c4947014287a69fa1b
Ami Pham <amipham@gmail.com>
2026-09-21 14:28:33 +0700

    feat: bai5 - Biến (variables)

---
+++ src/bai5_variables.java
@@ -0,0 +1,23 @@
+public class bai5_variables {
+    public static void main(String[] args) {
+        int a;
+        byte tuoi;
+        float diemToan;
+
+        //khai bao nhieu bien cung kieu
+        int b,c,d;
+
+        //khoi tao bien
+        int tuoiCon = 10;
+        float diemVan = 7.5f;
+        double diemAnh = 7.5;
+        tuoiCon = 15; // co the gan lai
+        System.out.println(tuoiCon);
+
+        //hang so
+        final int DOSOIH20 = 100;
+         //KO the dan lai cho hang so: DOSOIH20 = 400
+
+
+    }
+}
