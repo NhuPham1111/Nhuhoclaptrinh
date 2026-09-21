da86685b824e8872afce25b9cf883bb553a762d0
Ami Pham <amipham@gmail.com>
2026-09-21 14:28:34 +0700

    feat: bai7-8 - Các phép toán

---
+++ src/bai7_bai8_cacpheptoan.java
@@ -0,0 +1,34 @@
+public class bai7_bai8_cacpheptoan {
+    public static void main(String[] args) {
+        int a= 11;
+        int b = 7;
+        System.out.println("a= " + a);
+        System.out.println("b= " + b);
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a % b = " + (a % b));
+        System.out.println("a / b = " + (double)(a / b)); //ep kieu cho phep chia
+
+    }
+}
+//Toan tu gan
+/*
+* =
+* +=
+* -=
+* *=
+* /=
+* %=
+* */
+
+
+//Cac phep so sanh return true/false
+/*
+* ==
+* !=
+* >
+* <
+* >=
+* <=
+* */
