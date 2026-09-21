062f174aa6d334a06f857fcd47c9c6dbd50433de
Ami Pham <amipham@gmail.com>
2026-09-21 14:28:35 +0700

    feat: bai15 - Vòng lặp do-while

---
+++ src/bai15_do_white.java
@@ -0,0 +1,24 @@
+public class bai15_do_white {
+    public static void main(String[] args) {
+    // Do white khac voi while: no thi hanh khoi lenh truoc, sau do moi check dieu kien, neu dk dung thi loop tiep
+        // Vd: tinh tong cac so tu 1-5
+//        int a = 1;
+//        int sum = 0;
+//        do{
+//            sum += a;
+//            a++;
+//        }while (a<=5);
+//        System.out.println("tong = "+ sum);
+
+    // while true: luon dung ko co diem dung
+        //vd:
+        int n = 0;
+        while (true){
+            n++;
+            System.out.println("n = " + n);
+            if (n ==9) break;
+
+        }
+
+    }
+}
