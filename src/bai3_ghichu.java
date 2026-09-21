dd32aa1e503d222d1d5a109aa2cb9d4ffa4418f4
Ami Pham <amipham@gmail.com>
2026-09-21 14:28:32 +0700

    feat: bai3 - Ghi chú (comments)

---
+++ src/bai3_ghichu.java
@@ -0,0 +1,8 @@
+public class bai3_ghichu {
+    public static void main(String[] args) {
+        // ghi chu 1 dong
+       // System.out.println("dong1");
+        /* ghi chu nhieu dong
+         ghi chu nhieu dong*/
+    }
+}
