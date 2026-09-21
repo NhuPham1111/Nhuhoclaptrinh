14dfda7b74d4f325a7cac0e0780665d843d959fc
Ami Pham <amipham@gmail.com>
2026-09-21 14:28:33 +0700

    feat: bai4 - Kiểu dữ liệu

---
+++ src/bai4_kieudulieu.java
@@ -0,0 +1,8 @@
+public class bai4_kieudulieu {
+    public static void main(String[] args) {
+        // Kt kieu so nguyen
+        System.out.println("max of interger" + Integer.MAX_VALUE);
+        System.out.println("min of interger" + Integer.MIN_VALUE);
+
+    }
+}
