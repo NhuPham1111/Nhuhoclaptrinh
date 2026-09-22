e74ae4380f030bfc8d9faae0525121bfa654fcf6
Ami Pham <amipham@gmail.com>
2026-09-22 16:01:20 +0700

    bai26_4_vdvandungString

--- src/bai26_4_vdvandungString.java
+++ src/bai26_4_vdvandungString.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class bai26_4_vdvandungString {
+import java.util.Scanner;
+
+public class bai26_4_vdvandungString {
+    public static void main(String[] args) {
+        /*
+        Bai1: nhap vao 1 chuoi tu ban phim,
+        dem xem co bao nhieu ki tu thuong?,
+        bao nhieu ki tu in hoa?
+        bao nhieu so
+        bao nhieu space
+        * */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap vao 1 chuoi: ");
+        String s = sc.nextLine(); //doc du lieu tu ban phim va gan vao bien s
+        int demSo = 0;
+        int demChuThuong = 0;
+        int demChuInHoa= 0;
+        int demSpace = 0;
+        for (int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if (Character.isLowerCase(c)){
+                demChuThuong++;
+            } else if (Character.isUpperCase(c)) {
+                demChuInHoa++;
+
+            } else if (Character.isWhitespace(c)) {
+                demSpace++;
+            }else
+                demSo++;
+        }
+        System.out.println("So chu thuong la: "+demChuThuong);
+        System.out.println("So chu in hoa la: "+demChuInHoa);
+        System.out.println("So chu so: "+demSo);
+        System.out.println("So khoan trang: "+demSpace);
+
+
+    }
 }
